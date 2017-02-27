7c54b283593c81ee5b3ac0213798bea4605c5536
jenniechan12 <jenniechan123@gmail.com>
2017-02-27 17:08:55 -0500

    Add files via upload

---
+++ ROLS2D/Assets/Scripts/Character Classes/BaseCharacterClass.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCharacterClass{
+
+	// Basic Character Info
+	private string characterName; 
+	private string characterDescription;
+
+	// Basic Character Stats
+	private int vitality;	// Vitality - influence in HP or health
+	private int strength;	// Strength - influence in Attack and Damage 
+	private int agility;	// Agility - influence in Accuracy and Evasion 
+	private int luck; 		// Luck - influence in Crit and Flee 
+
+	// TODO: Basic Character Spiritual Animal w/ specializes attack 
+
+	public string CharacterName{
+		get{ return characterName; }
+		set{ characterName = value; } 
+	}
+
+	public string CharacterDescription{
+		get{ return characterDescription; }
+		set{ characterDescription = value; }
+	}
+
+	public int Vitality{
+		get{ return vitality;}
+		set{ vitality = value;}
+	}
+
+	public int Strength{
+		get{ return strength;}
+		set{ strength = value;}
+	}
+
+	public int Agility{
+		get{ return agility;}
+		set{ agility = value;}
+	}
+
+	public int Luck{
+		get{ return luck;}
+		set{ luck = value;}
+	}
+
+}
--- ROLS2D/Assets/Scripts/LoadManager.cs
+++ ROLS2D/Assets/Scripts/LoadManager.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LoadManager : MonoBehaviour {
 	public int loadStatus; 
-	public GameObject message; 
+	public GameObject message, yesButton, noButton; 
 	Text messageTxt;
 	bool deleteFile;
 
@@ -39,22 +40,30 @@ public class LoadManager : MonoBehaviour {
 		if (loadStatus == 1) {
 			message.SetActive (true);
 			messageTxt.text = "Are you sure you want to delete current game file?";
+			yesButton.SetActive (true);
+			noButton.SetActive (true);
 			if (deleteFile) {
 				loadStatus = 0;
+				SceneManager.LoadScene (2);
 			} else {
 				loadStatus = 1;
 			}
 		} else {
 			message.SetActive (true);
 			messageTxt.text = "Creating New Game..."; 
+			SceneManager.LoadScene (2);
 		}
 	}
 
 	public void YesButton(){
 		deleteFile = true;
+		yesButton.SetActive (false);
+		noButton.SetActive (false);
 	}
 
 	public void NoButton(){
 		deleteFile = false;
+		yesButton.SetActive (false);
+		noButton.SetActive (false);
 	}
 }
